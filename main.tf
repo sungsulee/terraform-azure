ade21e4e5b5c969010844f8a7c0a58ea192ee705
Sung.Lee04 <Sung.Lee@cuny.edu>
2026-06-20 16:20:14 -0400

    Removed HTTP security rule

--- main.tf
+++ main.tf
@@ -32,18 +32,6 @@ resource "azurerm_network_security_group" "wordpress" {
   resource_group_name = azurerm_resource_group.workload.name
 
   security_rule {
-    name                       = "Allow-HTTP"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
     name                       = "Allow-HTTPS"
     priority                   = 110
     direction                  = "Inbound"
